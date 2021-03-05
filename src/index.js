760580880469a09f5d200efc3c3fd6a7df24a9b4
frankfang <frankfang1990@gmail.com>
2021-03-05 16:08:38 +0800

    支持 Stylus

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import {a} from '@/a' // 直接引入
-import {JsxDemo} from './jsx-demo.jsx';
+import {JsxDemo} from './jsx-demo.jsx'
 import {x} from './ts-demo.ts'
-import {TsxDemo} from './tsx-demo.tsx';
-import '@/scss-demo.scss'
-import vars from '@/less-demo.less'
+import {TsxDemo} from './tsx-demo.tsx'
+import '@/stylus-demo.styl'
+import vars from '@/stylus-vars.styl'
+
 console.log(vars)
 const b = import('./b') // 动态引入，也按需加载
 
